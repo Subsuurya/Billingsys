6fbe0293d5571b52f25eb9ea7fa13723c5454917
Subsuurya <suurya090703@gmail.com>
2025-11-17 15:24:30 +0800

    17 nov

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 function Login() {
   return (
-    //this is a comment to show the difference between main branch file and new branch file
-    //this is another comment
     <div className="flex items-center justify-center h-screen">
       <div className="p-6 w-64 h-32 flex items-center justify-center rounded-lg shadow-lg border border-gray-200">
         Login Page Placeholder
