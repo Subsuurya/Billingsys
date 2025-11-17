77295300f4b4dfb62fee187c3b903f9636a949ca
Subsuurya <suurya090703@gmail.com>
2025-11-17 14:16:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="p-6 w-64 h-32 flex items-center justify-center rounded-lg shadow-lg border border-gray-200">
+        Login Page Placeholder
+      </div>
+    </div>
+  );
+}
+
+export default Login;
